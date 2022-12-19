c1e6c6fb0b9cb5b42ad99ff4a4e29f437c7d1d85
Eric Tune <etune@google.com>
2022-12-19 08:15:42 -0800

    Prepare to refactor main to lib

--- day1/prob1.rs
+++ day1/prob1.rs
@@ -5,26 +5,27 @@ use std::fs;
 // prob1 input.txt
 // prob1 test_input.txt
 fn main() {
+    const TOPN: usize = 1;
     let args: Vec<String> = env::args().collect();
-    //dbg!(args);
     let file_path = &args[1];
     let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let mut elf = 1;
+    
+    let mut vec = Vec::<i32>::new();  
     let mut totalcal: i32 = 0;
-    let mut maxcal: i32 = 0;
 
     for found in input.split("\n") {
         if found == "" {
-            elf += 1;
+            vec.push(totalcal);
             totalcal = 0;
         } else {
             totalcal += found.parse::<i32>().unwrap();
-            if totalcal > maxcal {
-                maxcal = totalcal
-            }
-            //println!("{} {} {}", found, elf, totalcal);
         }
     }
-
-    println!("elf {} has {} calories", elf, maxcal);
+    // Print calories per elf descending
+    vec.sort();
+    vec.reverse();
+    //println!("{:?}", &vec[0..=2]);
+    let vec2: Vec<i32> = vec[0..=TOPN].to_vec();
+    // Calories of top n elves.
+    println!("{}", vec2.iter().sum::<i32>());
 }
--- day1/prob2.rs
+++ day1/prob2.rs
@@ -5,27 +5,27 @@ use std::fs;
 // prob2 input.txt
 // prob2 test_input.txt
 fn main() {
+    const TOPN: usize = 3;
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     let mut vec = Vec::<i32>::new();
-    let mut _elf = 1;
     let mut totalcal: i32 = 0;
 
     for found in input.split("\n") {
         if found == "" {
-            _elf += 1;
             vec.push(totalcal);
             totalcal = 0;
         } else {
             totalcal += found.parse::<i32>().unwrap();
         }
     }
+    // Print calories per elf descending
     vec.sort();
     vec.reverse();
     //println!("{:?}", &vec[0..=2]);
-    let vec2: Vec<i32> = vec[0..=2].to_vec();
-    //println!("{:#?}", &vec2);
+    let vec2: Vec<i32> = vec[0..=TOPN].to_vec();
+    // Calories of top n elves.
     println!("{}", vec2.iter().sum::<i32>());
 }
